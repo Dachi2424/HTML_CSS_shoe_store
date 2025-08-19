27ee45be20ac86d0f488cf502ac266c228066721
Dachi Darjania <dachidar19@gmail.com>
2025-08-19 16:06:35 +0400

    davamate

---
+++ script.js
@@ -0,0 +1,30 @@
+
+// open/close cart logic
+function openCart(){
+  const cartBar = document.querySelector(".header__cart-section");
+  const darkBG = document.querySelector(".header__cart-dark-background");
+
+  cartBar.style.display = "unset";
+  darkBG.style.display = "unset";
+}
+function closeCart(){
+  const cartBar = document.querySelector(".header__cart-section");
+  const darkBG = document.querySelector(".header__cart-dark-background");
+
+  cartBar.style.display = "none";
+  darkBG.style.display = "none";
+}
+
+// open/close login logic
+function openLogin(){
+  const login = document.querySelector(".header__login-section");
+  login.style.display = "flex";
+
+  document.body.style.overflow = "hidden";
+}
+function closeLogin(){
+  const login = document.querySelector(".header__login-section");
+  login.style.display = "none";
+
+  document.body.style.overflow = "auto";
+}
